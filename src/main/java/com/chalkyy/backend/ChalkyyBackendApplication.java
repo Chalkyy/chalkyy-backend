1bee3cd997d25bc0e3ac8ebfcb95d53e33c7e993
OskarWiedeweg <dev.wiedeweg@gmail.com>
2024-09-07 17:04:39 +0200

    feat: initial commit

---
+++ src/main/java/com/chalkyy/backend/ChalkyyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chalkyy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalkyyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalkyyBackendApplication.class, args);
+	}
+
+}
